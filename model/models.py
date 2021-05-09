17f0cd81f5735630a8a3615a94099828dd6546f3
jeremy.zhang <zhangyue9306@gmail.com>
2021-05-09 20:27:12 +0800

    refactor

--- model/models.py
+++ model/models.py
@@ -37,6 +37,7 @@ def train_A2C(env_train, model_name, timesteps=25000):
     print('Training time (A2C): ', (end - start) / 60, ' minutes')
     return model
 
+
 def train_ACER(env_train, model_name, timesteps=25000):
     start = time.time()
     model = ACER('MlpPolicy', env_train, verbose=0)
@@ -62,15 +63,16 @@ def train_DDPG(env_train, model_name, timesteps=10000):
     end = time.time()
 
     model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
-    print('Training time (DDPG): ', (end-start)/60,' minutes')
+    print('Training time (DDPG): ', (end - start) / 60, ' minutes')
     return model
+
 
 def train_PPO(env_train, model_name, timesteps=50000):
     """PPO model"""
 
     start = time.time()
-    model = PPO2('MlpPolicy', env_train, ent_coef = 0.005, nminibatches = 8)
-    #model = PPO2('MlpPolicy', env_train, ent_coef = 0.005)
+    model = PPO2('MlpPolicy', env_train, ent_coef=0.005, nminibatches=8)
+    # model = PPO2('MlpPolicy', env_train, ent_coef = 0.005)
 
     model.learn(total_timesteps=timesteps)
     end = time.time()
@@ -79,9 +81,10 @@ def train_PPO(env_train, model_name, timesteps=50000):
     print('Training time (PPO): ', (end - start) / 60, ' minutes')
     return model
 
+
 def train_GAIL(env_train, model_name, timesteps=1000):
     """GAIL Model"""
-    #from stable_baselines.gail import ExportDataset, generate_expert_traj
+    # from stable_baselines.gail import ExportDataset, generate_expert_traj
     start = time.time()
     # generate expert trajectories
     model = SAC('MLpPolicy', env_train, verbose=1)
@@ -163,8 +166,8 @@ def run_ensemble_strategy(df, unique_trade_date, rebalance_window, validation_wi
     model_use = []
 
     # based on the analysis of the in-sample data
-    #turbulence_threshold = 140
-    insample_turbulence = df[(df.datadate<20151000) & (df.datadate>=20090000)]
+    # turbulence_threshold = 140
+    insample_turbulence = df[(df.datadate < 20151000) & (df.datadate >= 20090000)]
     insample_turbulence = insample_turbulence.drop_duplicates(subset=['datadate'])
     insample_turbulence_threshold = np.quantile(insample_turbulence.turbulence.values, .90)
 
@@ -179,14 +182,14 @@ def run_ensemble_strategy(df, unique_trade_date, rebalance_window, validation_wi
             # previous state
             initial = False
 
-        # Tuning trubulence index based on historical data
+        # Tuning turbulence index based on historical data
         # Turbulence lookback window is one quarter
-        end_date_index = df.index[df["datadate"] == unique_trade_date[i - rebalance_window - validation_window]].to_list()[-1]
-        start_date_index = end_date_index - validation_window*30 + 1
+        end_date_index = \
+        df.index[df["datadate"] == unique_trade_date[i - rebalance_window - validation_window]].to_list()[-1]
+        start_date_index = end_date_index - validation_window * 30 + 1
 
         historical_turbulence = df.iloc[start_date_index:(end_date_index + 1), :]
-        #historical_turbulence = df[(df.datadate<unique_trade_date[i - rebalance_window - validation_window]) & (df.datadate>=(unique_trade_date[i - rebalance_window - validation_window - 63]))]
-
+        # historical_turbulence = df[(df.datadate<unique_trade_date[i - rebalance_window - validation_window]) & (df.datadate>=(unique_trade_date[i - rebalance_window - validation_window - 63]))]
 
         historical_turbulence = historical_turbulence.drop_duplicates(subset=['datadate'])
 
@@ -241,7 +244,7 @@ def run_ensemble_strategy(df, unique_trade_date, rebalance_window, validation_wi
 
         print("======DDPG Training========")
         model_ddpg = train_DDPG(env_train, model_name="DDPG_10k_dow_{}".format(i), timesteps=10000)
-        #model_ddpg = train_TD3(env_train, model_name="DDPG_10k_dow_{}".format(i), timesteps=20000)
+        # model_ddpg = train_TD3(env_train, model_name="DDPG_10k_dow_{}".format(i), timesteps=20000)
         print("======DDPG Validation from: ", unique_trade_date[i - rebalance_window - validation_window], "to ",
               unique_trade_date[i - rebalance_window])
         DRL_validation(model=model_ddpg, test_data=validation, test_env=env_val, test_obs=obs_val)
@@ -265,7 +268,7 @@ def run_ensemble_strategy(df, unique_trade_date, rebalance_window, validation_wi
 
         ############## Trading starts ##############
         print("======Trading from: ", unique_trade_date[i - rebalance_window], "to ", unique_trade_date[i])
-        #print("Used Model: ", model_ensemble)
+        # print("Used Model: ", model_ensemble)
         last_state_ensemble = DRL_prediction(df=df, model=model_ensemble, name="ensemble",
                                              last_state=last_state_ensemble, iter_num=i,
                                              unique_trade_date=unique_trade_date,
